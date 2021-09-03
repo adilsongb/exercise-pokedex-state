71ecf5a190483a06aeb03851bc69a7377e9bdf6d
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-09-02 23:44:03 -0300

    Criada pokedex dinamica

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+// arquivo App.js, criado pelo create-react-app, modificado
 import React from 'react';
 import './App.css';
-import pokemons from './data';
-import Pokedex from './Pokedex';
+import Pokedex from './components/Pokedex'
+import listPokemons from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Pokedex</h1>
+        <Pokedex data={listPokemons}/>
+      </div>
+    );
+  }
 }
 
 export default App;
